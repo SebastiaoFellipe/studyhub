8a3c996fd04f277716750a2515dc317bdbd60df8
SebastiaoFellipe <sebaslopes369@gmail.com>
2025-06-26 13:48:35 -0300

    feat: configurar script de build unificado para frontend e backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import taskRoutes from "./routes/taskRoute.js";
 import authRoutes from "./routes/authRoute.js";
@@ -13,6 +14,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
+
 app.use(cors());
 app.use(express.json());
 
@@ -21,6 +24,14 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/notes", noteRoutes);
 app.use("/api/decks", flashcardDeckRoutes);
 app.use("/api", cardRoutes);
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     connectDB();
